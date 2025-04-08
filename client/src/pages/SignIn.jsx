61479036cc25ff670ee1424016d80ba88879d658
Rami <rami@smartcloudjo.com>
2025-04-08 06:09:23 +0300

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+
 export default function SignIn() {
   const [formData, setFormData] = useState();
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -21,19 +31,25 @@ export default function SignIn() {
         headers: { "content-type": "application/json" },
         body: JSON.stringify(formData),
       });
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart());
       const data = await res.json();
+
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
-        setError(null);
+        // setLoading(false);
+        // setError(data.message);
+
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
+      dispatch(signInSuccess(data));
+      // setLoading(false);
+      // setError(null);
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
+      // setLoading(false);
+      // setError(error.message);
     }
   };
   return (
