83cbbbcb049cd35b792d4e964d1443bb86c179e9
Rami <rami@smartcloudjo.com>
2025-04-26 18:46:01 +0300

    add onchange and onsubmit functionality to the search page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -72,30 +72,30 @@ export const getListings = async (req, res, next) => {
     let offer = req.query.offer;
 
     if (offer === undefined || offer === "false") {
-      offer = { $in: [true, false] };
+      offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
     if (furnished === undefined || furnished === false) {
-      furnished = { $in: [true, false] };
+      furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
     if (parking === undefined || parking === false) {
-      parking = { $in: [true, false] };
+      parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
-    if (type === undefined || parking === "all") {
-      type = { $in: ["rent", "sale"] };
+    if (type === undefined || type === "all") {
+      type = { $in: ["sell", "rent"] };
     }
 
-    const searchItem = req.query.searchItem || "";
-    const sort = req.query.sort || "createAt";
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
     const order = req.query.order || "desc";
 
     const listings = await Listing.find({
-      name: { $regex: searchItem, $options: "i" },
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
